72d10a0106fde1b01703d07a3f656796a218ca92
Ismagulov <na.ismagulov@gmail.com>
2022-03-29 02:08:57 +0300

    first commit

---
+++ Petrenko-Goltsman Index/IndexCalculation.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Petrenko_Goltsman_Index
+{
+    class IndexCalculation
+    {
+        public double indexPetrenkoRus(string text)
+        {
+            double index = 0.5d;
+
+            for (double i = 1.5d; i - 0.5d < text.Count(char.IsLetter); i++)
+            {
+                index = index + i;
+            }
+
+            index = index * text.Count(char.IsLetter);
+
+            return index;
+        }
+
+        public double indexPetrenkoEng(string text)
+        {
+            string[] textArray = text.Split("|");
+
+            double totalIndex = 0;
+
+            foreach (string obj in textArray)
+            {
+                double index = 0.5d;
+
+                for (double i = 1.5d; i - 0.5d < obj.Count(char.IsLetter); i++)
+                {
+                    index = index + i;
+                }
+
+                index = index * obj.Count(char.IsLetter);
+
+                totalIndex += index;
+            }
+
+            return totalIndex;
+        }
+    }
+}
---
+++ Petrenko-Goltsman Index/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace Petrenko_Goltsman_Index
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            IndexCalculation indexCalculation = new IndexCalculation();
+
+            Console.WriteLine("Введите строку на русском языке");
+
+            string rusString = Console.ReadLine();
+
+            Console.WriteLine("Введите строку на английском языке и комментарий");
+
+            string engString = Console.ReadLine();
+
+            double rusIndex = indexCalculation.indexPetrenkoRus(rusString);
+
+            double engIndex = indexCalculation.indexPetrenkoEng(engString);
+
+            Console.WriteLine("Индекс Петренко для строки на русском языке - " + rusIndex.ToString());
+
+            Console.WriteLine("Индекс Петренко для строки на английском языке и комментария - " + engIndex.ToString());
+
+            if (rusIndex == engIndex)
+            {
+                Console.WriteLine("Индекс Петренко для строки на русском языке и индекс Петренко для строки на"
+                                 + " английском языке и комментария совпадают. Их индекс: " + rusIndex.ToString());
+            }
+
+            Console.ReadKey();
+
+        }
+    }
+}
